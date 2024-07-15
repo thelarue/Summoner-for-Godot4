47952222820661fe503cdd3fad5c171450c36109
Ru <deannalovesbooks@gmail.com>
2024-07-14 23:19:06 -0400

    audio

--- scenes/starting_area/start.gd
+++ scenes/starting_area/start.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AudioPlayer.play_music_level()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
