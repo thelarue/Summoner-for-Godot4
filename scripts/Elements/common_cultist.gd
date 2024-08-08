41edaaf4290cd2fc42f604a9536baf3534aafdd4
ItStress <marconapolitano02@gmail.com>
2024-08-08 14:13:47 +0200

    Cultist pathfinding

--- scripts/Elements/common_cultist.gd
+++ scripts/Elements/common_cultist.gd
@@ -1,14 +1,32 @@
 extends CharacterBody2D
 
-@onready var player = get_tree().get_first_node_in_group("player")
+@export var target : Node2D = null
+
 @export var SPEED : float = 100.0
 @export var dmg : int  = 2
 @onready var anim = $AnimatedSprite2D
 
+@onready var navigation_agent_2d = $NavigationAgent2D
+
+func _ready():
+	call_deferred("seeker_setup")
+
 func _physics_process(_delta):
-	if player != null:
-		var direction = global_position.direction_to(player.global_position)	
-		velocity = SPEED * direction
+	if target:
+		navigation_agent_2d.target_position = target.global_position
+		
+	if navigation_agent_2d.is_navigation_finished():
+		return
+	
+	var current_agent_position = global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var new_velocity = current_agent_position.direction_to(next_path_position) * SPEED
+	
+	if navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity_forced(new_velocity)
+	else:
+		velocity = new_velocity
+	
 	var collisions = get_slide_collision_count()
 	for i in collisions:
 		var collision = get_slide_collision(i)
@@ -18,6 +36,12 @@ func _physics_process(_delta):
 	handle_animations()
 	move_and_slide()
 
+func seeker_setup():
+	await get_tree().physics_frame
+	if target:
+		navigation_agent_2d.target_position = target.global_position
+		
+	
 func handle_animations():
 	if velocity.length() == 0:
 		if anim.is_playing():
@@ -37,3 +61,5 @@ func handle_animations():
 		
 		anim.play("walk_" + dir)
 	
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
