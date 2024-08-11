d274a661f150bc889a95f27eae127aeac6718571
Balázs Nagy <bazsi1224@gmail.com>
2024-08-11 14:12:44 +0200

    half of the map explorable
    the rooms are created however not filled with anything
    other half will be done soon

--- scripts/Utils/NavigationManager.gd
+++ scripts/Utils/NavigationManager.gd
@@ -1,35 +1,31 @@
 extends Node
 
-const start = preload("res://scenes/starting_area/start.tscn")
-const second_area = preload("res://scenes/starting_area/second_area.tscn")
-const starting_room = preload("res://scenes/level 1/starting_room.tscn")
-const hallway_1 = preload("res://scenes/level 1/hallway_1.tscn")
-const hallway_2 = preload("res://scenes/level 1/hallway_2.tscn")
-const storage_room_1 = preload("res://scenes/level 1/storage_room_1.tscn") 
+const LEVELS : Dictionary = {
+	"start"                : preload("res://scenes/starting_area/start.tscn"),
+	"second_area"          : preload("res://scenes/starting_area/second_area.tscn"),
+	"starting_room"        : preload("res://scenes/level 1/starting_room.tscn"),
+	"hallway_1"            : preload("res://scenes/level 1/hallway_1.tscn"),
+	"hallway_2"            : preload("res://scenes/level 1/hallway_2.tscn"),
+	"hallway_3"            : preload("res://scenes/level 1/hallway_3.tscn"),
+	"storage_room_1"       : preload("res://scenes/level 1/storage_room_1.tscn"),
+	"southern_hallway"     : preload("res://scenes/level 1/southern_hallway.tscn"),
+	"toilets"              : preload("res://scenes/level 1/toilets.tscn"),
+	"livestock_room"       : preload("res://scenes/level 1/livestock_room.tscn")
+}
+
+
+
 
 signal on_trigger_player_spawn
 
 var spawn_door_tag 
 
 func go_to_level(level_tag, destination_tag):
-	var scene_to_load
-	match level_tag:
-		"start":
-			scene_to_load = start
-		"second_area":
-			scene_to_load = second_area
-		"starting_room":
-			scene_to_load = starting_room
-		"hallway_1":
-			scene_to_load = hallway_1
-		"hallway_2":
-			scene_to_load = hallway_2
-		"storage_room_1":
-			scene_to_load = storage_room_1
-		
-	if scene_to_load != null:
-		spawn_door_tag = destination_tag
-		call_deferred("change_scene", scene_to_load)
+	if not LEVELS.keys().has( level_tag ): assert( "Unkown target level" )
+	
+	var scene_to_load = LEVELS[level_tag]
+	spawn_door_tag = destination_tag
+	call_deferred("change_scene", scene_to_load)
 
 func change_scene(scene_to_load):
 	get_tree().change_scene_to_packed(scene_to_load)
--- scripts/Utils/PuzzleCompletionList.gd
+++ scripts/Utils/PuzzleCompletionList.gd
@@ -1,5 +1,8 @@
 extends Node
 
+
+# before submiting set all these to false
 var puzzles : Dictionary = {
-	"blood_door" : false
+	"blood_door" : true,
+	"bronze_door" : true,
 }
