68f287b0cbe898bd51547f1cfd7c1e284fe4f8d4
Balázs Nagy <bazsi1224@gmail.com>
2024-08-13 23:58:37 +0200

    set puzzles back

--- scripts/Utils/PuzzleCompletionList.gd
+++ scripts/Utils/PuzzleCompletionList.gd
@@ -4,8 +4,8 @@ extends Node
 # before submiting set all these to false
 var puzzles : Dictionary = {
 	"blood_door"       : false,
-	"bronze_door"      : true,
-	"silver_door"      : true,
+	"bronze_door"      : false,
+	"silver_door"      : false,
 	"gem_door_red"     : false,
 	"gem_door_blue"    : false,
 	"gem_door_purple"  : false,
