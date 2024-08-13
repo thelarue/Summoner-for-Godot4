f5ed8df050967f9603fbaaa2d952df6a53d82552
ItStress <marconapolitano02@gmail.com>
2024-08-13 08:57:31 +0200

    Nature summon ability visuals

--- scripts/Summons/NatureSummon.gd
+++ scripts/Summons/NatureSummon.gd
@@ -1,11 +1,8 @@
 extends Summon
 
-
 func overworld_ability():
 	var player_node = Global.get_player_node()
 	if player_node:
 		var grass_area = player_node.get_closest_grass_area()
 		if grass_area:
-			grass_area.get_node("ColorRect").visible = true
-			grass_area.get_node("StaticBody2D/CollisionShape2D").disabled = true
-			Global.player_mana -= mana_cost
+			grass_area.grow(mana_cost)
--- scripts/grass_area.gd
+++ scripts/grass_area.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+@onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d : CollisionShape2D = $StaticBody2D/CollisionShape2D
+
+
+var active = false
+
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		body.set_closest_grass_area(self)
+
+func grow(mana_cost : int):
+	if not active:
+		animated_sprite_2d.play("growth")
+		collision_shape_2d.disabled = true
+		active = true
+		Global.player_mana -= mana_cost
