e7f58981c07d5f7b8a9203eda57df65e817df9bb
Balázs Nagy <97000153+Bazsi1224@users.noreply.github.com>
2024-08-14 11:22:07 +0200

    audio effects added

--- audio_player.gd
+++ audio_player.gd
@@ -1,6 +1,5 @@
 extends AudioStreamPlayer
 
-
 const level_music = preload("res://assets/music and sound/great_beholder_preview_wip.mp3")
 
 func _play_music(music: AudioStream, volume = -20.0):
@@ -11,4 +10,10 @@ func _play_music(music: AudioStream, volume = -20.0):
 	play()
 	
 func play_music_level():
-	_play_music(level_music)
+	if not playing:
+		finished.connect( play_music_level )
+		_play_music(level_music)
+
+
+func loop_music():
+	play_music_level()
